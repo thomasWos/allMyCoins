5bdbb11ad2f4f5c316e357d652a22d6dcb782eba
Thomas Wostyn <thomas.wostyn@gmail.com>
2020-10-18 16:58:22 +1100

    request // for Elrond provider

--- src/main/java/com/allmycoins/balance/ElrondProvider.java
+++ src/main/java/com/allmycoins/balance/ElrondProvider.java
@@ -2,6 +2,7 @@ package com.allmycoins.balance;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Future;
 
 import com.allmycoins.PrivateConfig;
 import com.allmycoins.json.BalanceJson;
@@ -10,6 +11,7 @@ import com.allmycoins.json.elrond.ElrondDelegationJson;
 import com.allmycoins.pc.BuildElrondBalance;
 import com.allmycoins.request.elrond.ElrondAddressBalanceRequest;
 import com.allmycoins.request.elrond.ElrondDelegationRequest;
+import com.allmycoins.utils.FutureUtils;
 import com.allmycoins.utils.RequestUtils;
 
 public class ElrondProvider implements BalanceProvider {
@@ -20,10 +22,16 @@ public class ElrondProvider implements BalanceProvider {
 	}
 
 	private List<BalanceJson> balances(String eldondAddress) {
-		ElrondBalanceRequestJson elrondBalanceRequestJson = RequestUtils
-				.sendRequest(new ElrondAddressBalanceRequest(eldondAddress));
-		ElrondDelegationJson elrondDelegationJson = RequestUtils
-				.sendRequest(new ElrondDelegationRequest(eldondAddress));
+
+		Future<ElrondBalanceRequestJson> elrondBalanceRequestJsonF = FutureUtils
+				.runCallable(() -> RequestUtils.sendRequest(new ElrondAddressBalanceRequest(eldondAddress)));
+
+		Future<ElrondDelegationJson> elrondDelegationJsonF = FutureUtils
+				.runCallable(() -> RequestUtils.sendRequest(new ElrondDelegationRequest(eldondAddress)));
+
+		ElrondBalanceRequestJson elrondBalanceRequestJson = FutureUtils.futureResult(elrondBalanceRequestJsonF);
+		ElrondDelegationJson elrondDelegationJson = FutureUtils.futureResult(elrondDelegationJsonF);
+
 		BalanceJson elrondBalance = BuildElrondBalance.build(elrondBalanceRequestJson, elrondDelegationJson);
 		return List.of(elrondBalance);
 	}
