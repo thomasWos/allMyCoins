6f4a9bb103993f463471dc4d9a7e200d3877fa5f
Thomas Wostyn <thomas.wostyn@gmail.com>
2021-04-13 18:45:40 +1000

    Remove legacy elrond delegation

--- src/main/java/com/allmycoins/balance/elrond/ElrondProvider.java
+++ src/main/java/com/allmycoins/balance/elrond/ElrondProvider.java
@@ -1,9 +1,14 @@
 package com.allmycoins.balance.elrond;
 
+import static java.math.BigDecimal.ZERO;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.concurrent.Future;
 
 import com.allmycoins.balance.PublicAddressSingleBalanceProvider;
 import com.allmycoins.json.BalanceJson;
+import com.allmycoins.utils.BigDecimalUtils;
 import com.allmycoins.utils.FutureUtils;
 import com.allmycoins.utils.RequestUtils;
 
@@ -19,17 +24,17 @@ public final class ElrondProvider implements PublicAddressSingleBalanceProvider 
 		Future<ElrondBalanceRequestJson> elrondBalanceRequestJsonF = RequestUtils
 				.sendRequestFuture(new ElrondAddressBalanceRequest(publicAddress));
 
-		Future<ElrondLegacyDelegationJson> elrondLegacyDelegationJsonF = RequestUtils
-				.sendRequestFuture(new ElrondLegacyDelegationRequest(publicAddress));
-
 		Future<ElrondDelegationJson[]> elrondDelegationsJsonF = RequestUtils
 				.sendRequestFuture(new ElrondDelegationsRequest(publicAddress));
 
 		ElrondBalanceRequestJson elrondBalanceRequestJson = FutureUtils.futureResult(elrondBalanceRequestJsonF);
-		ElrondLegacyDelegationJson elrondLegacyDelegationJson = FutureUtils.futureResult(elrondLegacyDelegationJsonF);
 		ElrondDelegationJson[] elrondDelegationsJson = FutureUtils.futureResult(elrondDelegationsJsonF);
 
-		return BuildElrondBalance.build(elrondBalanceRequestJson, elrondLegacyDelegationJson, elrondDelegationsJson);
-	}
+		BigDecimal delegation = Arrays.stream(elrondDelegationsJson)
+				.map(d -> d.getUserActiveStake().add(d.getClaimableRewards())).reduce(ZERO, (d1, d2) -> d1.add(d2));
 
+		BigDecimal allElronds = elrondBalanceRequestJson.getData().getBalance().add(delegation);
+		float qty = BigDecimalUtils.decimal18(allElronds);
+		return new BalanceJson("EGLD", qty, "Elrond wallet");
+	}
 }
