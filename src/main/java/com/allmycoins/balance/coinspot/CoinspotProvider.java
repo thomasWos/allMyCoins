5ef5d39bc0ef9a0e988c313660d8ba52044ba0ff
Thomas Wostyn <thomas.wostyn@gmail.com>
2021-05-22 15:06:36 +1000

    Clean coinspot

--- src/main/java/com/allmycoins/balance/coinspot/CoinspotProvider.java
+++ src/main/java/com/allmycoins/balance/coinspot/CoinspotProvider.java
@@ -1,8 +1,12 @@
 package com.allmycoins.balance.coinspot;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import com.allmycoins.PrivateConfig;
 import com.allmycoins.balance.BalanceProvider;
@@ -10,6 +14,11 @@ import com.allmycoins.json.BalanceJson;
 import com.allmycoins.utils.RequestUtils;
 
 public final class CoinspotProvider implements BalanceProvider {
+
+	private final Predicate<Entry<String, CoinspotBalanceJson>> IS_AUD_BALANCE = e -> e.getKey().equals("AUD");
+
+	private final Predicate<Entry<String, CoinspotBalanceJson>> POSITIVE_BALANCE = e -> e.getValue()
+			.getAudbalance() > 0.01;
 
 	@Override
 	public List<BalanceJson> balances() {
@@ -22,10 +31,19 @@ public final class CoinspotProvider implements BalanceProvider {
 
 			CoinspotBalancesJson coinspotBalancesJson = RequestUtils
 					.sendRequest(new CoinspotBalancesRequest(apiKey, secretKey));
-			return BuildCoinspotBalances.build(coinspotBalancesJson);
-		}
 
+			return build(coinspotBalancesJson);
+		}
 		return Collections.emptyList();
 	}
 
+	public final List<BalanceJson> build(CoinspotBalancesJson coinspotBalancesJson) {
+		return coinspotBalancesJson.getBalances().stream().map(m -> m.entrySet().iterator().next())
+				.filter(IS_AUD_BALANCE.negate().and(POSITIVE_BALANCE)).map(e -> toBalalance(e.getKey(), e.getValue()))
+				.collect(toList());
+	}
+
+	private BalanceJson toBalalance(String symbol, CoinspotBalanceJson coinspotBalanceJson) {
+		return new BalanceJson(symbol, coinspotBalanceJson.getBalance(), "Coinspot");
+	}
 }
