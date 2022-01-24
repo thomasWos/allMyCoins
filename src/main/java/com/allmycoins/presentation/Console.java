df0f6a838201eea64c4a11387827084aee136c75
Samuel Tessian <tessian.samuel@gmail.com>
2022-01-24 12:56:58 +1100

    Filtering out null values upon display

--- src/main/java/com/allmycoins/presentation/Console.java
+++ src/main/java/com/allmycoins/presentation/Console.java
@@ -4,6 +4,7 @@ import static java.util.Comparator.reverseOrder;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 import com.allmycoins.datatype.Asset;
 import com.allmycoins.datatype.Balance;
@@ -14,11 +15,15 @@ public final class Console {
 	private static final Comparator<Asset> ASSET_COMP = Comparator.comparing(a -> a.getBalance().getCurrencyValue(),
 			reverseOrder());
 
+	private static final Predicate<Asset> ASSET_NOT_WORTH_ZERO = a -> a.getBalance().getCurrencyValue() >= 0.01f
+			|| a.getBalance().getCurrencyValue() <= -0.01f;
+
 	public static void display(final BalancesResult balancesResult, final String pCurrency) {
 		String currency = pCurrency.toUpperCase();
 
 		TableConsole tableConsole = new TableConsole(List.of("Asset", "Qty", "Balance", "%", "Rank", "From"));
-		balancesResult.getAssets().stream().sorted(ASSET_COMP).forEach(b -> addTableRow(b, currency, tableConsole));
+		balancesResult.getAssets().stream().sorted(ASSET_COMP).filter(ASSET_NOT_WORTH_ZERO)
+				.forEach(b -> addTableRow(b, currency, tableConsole));
 
 		System.out.println(tableConsole.toString());
 		System.out.println("Total: " + String.format("%.2f", balancesResult.getTotalCurrency()) + " " + currency);
