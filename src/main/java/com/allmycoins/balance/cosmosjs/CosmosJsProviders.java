5f1d99e4cc6708951c4ff16fed74d032fd8b90c3
Samuel Tessian <tessian.samuel@gmail.com>
2022-01-24 12:19:16 +1100

    New cosmos providers

--- src/main/java/com/allmycoins/balance/cosmosjs/CosmosJsProviders.java
+++ src/main/java/com/allmycoins/balance/cosmosjs/CosmosJsProviders.java
@@ -8,7 +8,14 @@ public final class CosmosJsProviders {
 			new CosmosJsProvider("OSMOSIS_ADDRESS", "osmosis", "uosmo", "OSMO"),
 			new CosmosJsProvider("AKASH_ADDRESS", "akash", "uakt", "AKT"),
 			new CosmosJsProvider("IRISNET_ADDRESS", "iris", "uiris", "IRIS"),
-			new CosmosJsProvider("JUNO_ADDRESS", "juno", "ujuno", "JUNO"));
+			new CosmosJsProvider("JUNO_ADDRESS", "juno", "ujuno", "JUNO"),
+			new CosmosJsProvider("COMDEX_ADDRESS", "comdex", "ucmdx", "CMDX"),
+			new CosmosJsProvider("CHIHUAHUA_ADDRESS", "chihuahua", "uhuahua", "HUAHUA"),
+			new CosmosJsProvider("STARGAZE_ADDRESS", "stargaze", "ustars", "STARS"),
+			new CosmosJsProvider("REGEN_ADDRESS", "regen", "uregen", "REGEN"),
+			new CosmosJsProvider("EMONEY_ADDRESS", "emoney", "ungm", "NGM"),
+			new CosmosJsProvider("LUM_ADDRESS", "lum", "ulum", "LUM"),
+			new CosmosJsProvider("DESMOS_ADDRESS", "desmos", "udsm", "DSM"));
 
 	public static final List<CosmosJsProvider> providers() {
 		return PROVIDERS;
